edd28997d8d1adae02fb44d0698bafbc990b25f0
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-01-31 16:10:28 -0200

    Update e.php

--- e.php
+++ e.php
@@ -1,15 +1,10 @@
 <?php
 //Criado por Anderson Ismael
 //31 de janeiro de 2019
-function e(string $text, bool $trim=true){
-    if ($trim) {
-        $text = preg_replace("/[\r\n]+/", " ", $text);
-        $text = preg_replace("/\s+/", ' ', $text);
-        $text = preg_replace("/\t+/", ' ', $text);
-        $text = trim($text);
-        $text = htmlentities($text);
+function e(string $text, bool $print=true){
+    if ($print) {
+        print htmlentities($text);
     } else {
-        $text = htmlentities($text);
+        return htmlentities($text);
     }
-    return $text;
 }
